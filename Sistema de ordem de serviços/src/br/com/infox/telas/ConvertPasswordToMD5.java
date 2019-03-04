929a8dd00627ecaa2ec71da592922e255af7c01e
Josemar <junioraguiar255@gmail.com>
2019-03-04 01:14:07 -0300

    Inclusão da Classe convertPassword

--- Sistema de ordem de serviços/src/br/com/infox/telas/ConvertPasswordToMD5.java
+++ Sistema de ordem de serviços/src/br/com/infox/telas/ConvertPasswordToMD5.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+
+//Classe para criptografia
 public class ConvertPasswordToMD5 {
 	public static String convertPasswordToMD5(String password) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("MD5");
