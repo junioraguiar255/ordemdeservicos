c5715571a4e5d02b2daebfe7cd8207272446f3f4
Josemar <junioraguiar255@gmail.com>
2019-03-04 01:05:27 -0300

    Inclusão da classe de criptografia

---
+++ Sistema de ordem de serviços/src/br/com/infox/telas/ConvertPasswordToMD5.java
@@ -0,0 +1,16 @@
+package br.com.infox.telas;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class ConvertPasswordToMD5 {
+	public static String convertPasswordToMD5(String password) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+
+		BigInteger hash = new BigInteger(1, md.digest(password.getBytes()));
+
+		return String.format("%32x", hash);
+	}
+
+}
